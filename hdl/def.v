9f18f7250c588fbebcd26ef5d3d375b1d6e8f272
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-06-11 02:09:58 +0800

    Add definitions

---
+++ hdl/def.v
@@ -0,0 +1,19 @@
+`ifndef _DEF_V
+`define _DEF_V
+
+// Common Definitions
+`define DATA_WIDTH 16    // A data is a 16-bit fixed-point number
+`define WORD_WIDTH 128   // A word in global buffer has 8 of data
+`define ADDR_WIDTH 12    // Global buffer has 4096 entries
+
+// Data positions in a word (little endian)
+`define DATA7 127:112
+`define DATA6 111:96
+`define DATA5 95:80
+`define DATA4 79:64
+`define DATA3 63:48
+`define DATA2 47:32
+`define DATA1 31:16
+`define DATA0 15:0
+
+`endif
