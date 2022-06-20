512352f164a64d907f1d203b8673bc52c17a0d51
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-06-20 10:05:55 +0800

    Fix incorrect input setup sequence

--- hdl/systolic_input_setup.v
+++ hdl/systolic_input_setup.v
@@ -36,8 +36,8 @@ module systolic_input_setup (
   reg [`DATA_WIDTH-1:0]   buf_q7;  // 7-cycle delay
 
   assign skew_o = {
-    word_i[`DATA7], buf_q1[`DATA6], buf_q2[`DATA5], buf_q3[`DATA4],
-    buf_q4[`DATA3], buf_q5[`DATA2], buf_q6[`DATA1], buf_q7[`DATA0]
+    buf_q7[`DATA0], buf_q6[`DATA0], buf_q5[`DATA0], buf_q4[`DATA0], 
+    buf_q3[`DATA0], buf_q2[`DATA0], buf_q1[`DATA0], word_i[`DATA0]
   };
 
   always @(posedge clk_i or negedge rst_ni) begin
@@ -51,13 +51,13 @@ module systolic_input_setup (
       buf_q7 <= 'd0;
     end else begin
       if (en_i) begin
-        buf_q1 <= word_i[`DATA_WIDTH*7-1:0];
-        buf_q2 <= buf_q1[`DATA_WIDTH*6-1:0];
-        buf_q3 <= buf_q2[`DATA_WIDTH*5-1:0];
-        buf_q4 <= buf_q3[`DATA_WIDTH*4-1:0];
-        buf_q5 <= buf_q4[`DATA_WIDTH*3-1:0];
-        buf_q6 <= buf_q5[`DATA_WIDTH*2-1:0];
-        buf_q7 <= buf_q6[`DATA_WIDTH-1:0];
+        buf_q1 <= word_i[`WORD_WIDTH-1:`DATA_WIDTH];
+        buf_q2 <= buf_q1[`DATA_WIDTH*7-1:`DATA_WIDTH];
+        buf_q3 <= buf_q2[`DATA_WIDTH*6-1:`DATA_WIDTH];
+        buf_q4 <= buf_q3[`DATA_WIDTH*5-1:`DATA_WIDTH];
+        buf_q5 <= buf_q4[`DATA_WIDTH*4-1:`DATA_WIDTH];
+        buf_q6 <= buf_q5[`DATA_WIDTH*3-1:`DATA_WIDTH];
+        buf_q7 <= buf_q6[`DATA_WIDTH*2-1:`DATA_WIDTH];
       end
     end
   end
