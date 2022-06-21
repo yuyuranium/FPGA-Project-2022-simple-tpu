25ac756e2f8d4686990bade379ee5c2c5c951084
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-06-21 17:32:16 +0800

    Use 32-bit data and 14-bit addr for PS port

--- hdl/global_buffer.v
+++ hdl/global_buffer.v
@@ -15,15 +15,15 @@
 
 module global_buffer (
 
-  // A port
+  // A port for PS
   input clk_a_i,
   input we_a_i,  // Write enable A
   input en_a_i,  // Enable A
-  input      [`ADDR_WIDTH-1:0] addr_a_i,
-  input      [`WORD_WIDTH-1:0] wdata_a_i,
-  output reg [`WORD_WIDTH-1:0] rdata_a_o,
+  input      [`ADDR_WIDTH+2-1:0] addr_a_i,
+  input      [`WORD_WIDTH/4-1:0] wdata_a_i,
+  output reg [`WORD_WIDTH/4-1:0] rdata_a_o,
 
-  // B port
+  // B port for TPU
   input clk_b_i,
   input we_b_i,  // Write enable B
   input en_b_i,  // Enable B
@@ -36,20 +36,40 @@ module global_buffer (
   reg [`WORD_WIDTH-1:0] gbuff [`GBUFF_ADDR_BEGIN:`GBUFF_ADDR_END];
 
   // Global buffer read/write behavior
+  wire [`ADDR_WIDTH-1:0] addr_a_base   = addr_a_i[`ADDR_WIDTH+2-1:2];
+  wire [1:0]             addr_a_offset = addr_a_i[1:0];
   
   // A port
   always @(posedge clk_a_i) begin
     if (en_a_i) begin
       if (we_a_i) begin
-        gbuff[addr_a_i] <= wdata_a_i;
-        rdata_a_o       <= wdata_a_i;
+        case (addr_a_offset)
+          2'b00:
+            gbuff[addr_a_base][31:0]   <= wdata_a_i;
+          2'b01:
+            gbuff[addr_a_base][63:32]  <= wdata_a_i;
+          2'b10:
+            gbuff[addr_a_base][95:64]  <= wdata_a_i;
+          2'b11:
+            gbuff[addr_a_base][127:96] <= wdata_a_i;
+        endcase
+        rdata_a_o <= wdata_a_i;
       end else begin
-        rdata_a_o <= gbuff[addr_a_i];
+        case (addr_a_offset)
+          2'b00:
+            rdata_a_o <= gbuff[addr_a_base][31:0];
+          2'b01:
+            rdata_a_o <= gbuff[addr_a_base][63:32];
+          2'b10:
+            rdata_a_o <= gbuff[addr_a_base][95:64];
+          2'b11:
+            rdata_a_o <= gbuff[addr_a_base][127:96];
+        endcase
       end
     end
   end
 
-  //B port
+  // B port
   always @(posedge clk_b_i) begin
     if (en_b_i) begin
       if (we_b_i) begin
@@ -60,7 +80,6 @@ module global_buffer (
       end
     end
   end
-
 
 endmodule
 
